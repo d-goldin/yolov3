3e9d09bb4feeea077c7cf5ccdea0e8a2363005bc
Dima <dgoldin+github@protonmail.ch>
2019-12-13 17:55:53 +0100

    Trying to get opencv and torchvision to do the CUDA

--- nix/default.nix
+++ nix/default.nix
@@ -2,16 +2,34 @@ self: super:
 
 rec {
   python37 = super.python37.override {
-     packageOverrides = py-self: py-super: {
-       pytorch = py-self.callPackage ./pytorch.nix {
-         cudaSupport = true;
-         cudnn = super.cudnn_cudatoolkit_10;
-         cudatoolkit = super.cudatoolkit_10;
-       };
-       torchvision = (py-self.callPackage ./torchvision.nix {
-       }).overrideAttrs (old: { FORCE_CUDA=1; CUDA_HOME="${super.cudatoolkit_10}"; });
-     };
-   };
+    packageOverrides = py-self: py-super: {
+      pytorch = py-self.callPackage ./pytorch.nix {
+        cudaSupport = true;
+        cudnn = super.cudnn_cudatoolkit_10;
+        cudatoolkit = super.cudatoolkit_10;
+      };
 
-   python37Packages = python37.pkgs;
- }
+      torchvision = (py-self.callPackage ./torchvision.nix { }).overrideAttrs
+        (old: {
+          #FORCE_CUDA=1;
+          CUDA_HOME = "${super.cudatoolkit_10}";
+          CUDNN_INCLUDE_DIR = "${super.cudnn_cudatoolkit_10}/include";
+          CUDNN_LIBRARY = "${super.cudnn_cudatoolkit_10}/lib";
+
+          buildInputs = old.buildInputs
+            ++ [ super.cudatoolkit_10 super.cudnn_cudatoolkit_10 ];
+        });
+    };
+  };
+
+  python37Packages = python37.pkgs;
+
+  opencv4 = (super.opencv4.override {
+    enableCuda = true;
+    cudatoolkit = super.cudatoolkit_10;
+  }).overrideAttrs (old: {
+    cmakeFlags = old.cmakeFlags ++ [
+      "-DBUILD_opencv_cudaoptflow=OFF"
+    ]; # Properietary SDK
+  });
+}
